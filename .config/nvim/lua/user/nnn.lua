a00faa3d5c907911517b1060251f3467463ca590
Mohamed <mosaleh@mail.com>
2023-12-14 21:35:21 +0200

    remove neovim config and add i3wm,i3status config ,modified sxhkdrc to work with i3

--- .config/nvim/lua/user/nnn.lua
+++
@@ -1,9 +0,0 @@
-
-local status_ok, nnn = pcall(require, "nnn")
-if not status_ok then
-  return
-end
-nnn.setup(
-    {    auto_close = false,
-    })
-
--- .config/nvim/lua/user/taskwiki.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, comment = pcall(require, "taskwiki")
-if not status_ok then
-  return
-end
-
-
